98420a52a72e8bed626398a30392dbfb47a638c2
r@m <richmedina@gmail.com>
2017-08-09 10:23:52 -1000

    minor ui adjusts

--- lltsite/admin.py
+++ lltsite/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 
 from .models import StoryPage, Dissertation
 
+class ExtraMedia:
+    js = [
+        '/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
+        '/static/js/tinymce_setup.js',
+    ]
 
 class StoryPageAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'featured', 'get_absolute_url')
@@ -12,5 +17,5 @@ class DissertationAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publish_date')
 
 
-admin.site.register(StoryPage, StoryPageAdmin)
-admin.site.register(Dissertation, DissertationAdmin)
+admin.site.register(StoryPage, StoryPageAdmin, Media = ExtraMedia)
+admin.site.register(Dissertation, DissertationAdmin) 
